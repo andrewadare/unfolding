edf7c4a611fe48450808be4f32405bd05bb54ca2
Andrew Adare <andrewadare@gmail.com>
2012-11-09 21:55:02 -0700

    random GSVD tester

---
+++ StressTestGSVD.C
@@ -0,0 +1,65 @@
+void StressTestGSVD()
+{
+  // Quotient SVD of A(m,n), B(p,n) where m >= n >= p
+  
+  gROOT->LoadMacro("UnfoldingUtils.C+");
+  UnfoldingUtils uu;
+  TRandom3 ran;
+
+  ran.SetSeed(0);
+  
+  int t=1, ntests=100;
+  while (t<=ntests) {
+
+    int p = 3 + ran.Integer(50);
+    int n = p + ran.Integer(10);
+    int m = n + ran.Integer(50);
+
+    //  int m=6, n=4, p=3;
+
+    Printf("\n\nGSVD Test %d: m=%d, n=%d, p=%d.", t,m,n,p);
+
+    TMatrixD A(m,n);
+    TMatrixD B(p,n);
+    for (int i=0; i<m; i++)
+      for (int j=0; j<n; j++)
+	A(i,j) = ran.Integer(9);
+    for (int i=0; i<p; i++)
+      for (int j=0; j<n; j++)
+	B(i,j) = ran.Integer(9);
+  
+    GSVDecompResult g = uu.GSVD(A,B);
+
+    TMatrixD upper = g.U*g.C*g.XT;
+    TMatrixD lower = g.V*g.S*g.XT;
+
+    TMatrixD M1(m+p,n);
+    M1.SetSub(0,0,A);
+    M1.SetSub(m,0,B);
+
+    TMatrixD M2(M1);
+    M2.SetSub(0,0,upper);
+    M2.SetSub(upper.GetNrows(),0,lower);
+    
+    M1 -= M2;
+
+    double sum = M1.Sum()/M1.GetNoElements();
+    if (sum < 1e-13) 
+      sum = 0.;
+    // cout << "A: "; A.Print();
+    // cout << "B: "; B.Print();
+
+    // cout << "U*C*XT: ";  upper.Print();
+    // cout << "V*S*XT: ";  lower.Print();
+
+    // cout << "M1: "; M1.Print();
+    // cout << "M2: "; M2.Print();
+
+    Printf("M1-M2 = %g", sum);
+    if (sum > 0) 
+      M1.Print();
+    
+    t++;
+  }
+
+}
