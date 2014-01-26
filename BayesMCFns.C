b918cf21e629c24e2668fdefd6f665d606611a56
Andrew Adare <andrewadare@gmail.com>
2014-01-26 13:22:20 -0700

    factored some logic from SampleMH() to AssignProposal() and AcceptProposal()

--- BayesMCFns.C
+++ BayesMCFns.C
@@ -1,4 +1,4 @@
-// *BayesMCFns.C*
+// BayesMCFns.C
 //
 // Collection of utility functions implementing the methods described
 // in "Fully Bayesian Unfolding" by G. Choudalakis (arXiv:1201.4612v4)
@@ -17,6 +17,7 @@
 #include "TMath.h"
 #include "TTree.h"
 #include "TRandom3.h"
+#include "ObjectiveFns.h"
 
 #include <iostream>
 
@@ -41,8 +42,6 @@ struct BayesianCredibilityInterval
   double probRequested, probComputed;
 
   TGraphErrors cdf;
-
-
 };
 
 // Function prototypes
@@ -51,17 +50,14 @@ TTree *
 SampleUniform(int nSamples, TVectorD &D, TMatrixD &Prt,
               TGraphAsymmErrors *box);
 TTree *
-SampleMH(int nSamples, int nBurnIn, TVectorD &D, TMatrixD &Prt,
-         TGraphAsymmErrors *box, double alpha, TVectorD &Tmc);
-double PoissonLikelihood(TVectorD &b, TVectorD &R);
-double LogPoissonLikelihood(TVectorD &b, TVectorD &R);
-double LogPoisson(double x, double mu);
-double LogGaussian(double x, double mu, double sigma, bool norm);
-double LogFactorial(int n);
+SampleMH(int nSamples, int nBurnIn, TMatrixD &Prt, TGraphAsymmErrors *box, 
+         LogLikeFn &llfunc, LogPrior &priorfunc);
+void AssignProposal(const TGraphAsymmErrors *box, const TVectorD &currentvec,
+                    TVectorD &newvec);
+bool AcceptProposal(double p0, double p1);
+
 void PrintPercentDone(int i, int N, int k);  // Print i/N (in %) every k%.
 BayesianCredibilityInterval GetBCI(TH1 *hp, double probFrac);
-
-
 
 TGraphAsymmErrors *
 HyperBox(TH1D *h)
@@ -73,7 +69,6 @@ HyperBox(TH1D *h)
   double min,max,mid;
   for (int t=0; t<Nt; t++)
   {
-
     mid = h->GetBinContent(t+1);
     min = 1./(t+10) * mid;
     if (min < 0)
@@ -99,7 +94,7 @@ SampleUniform(int nSamples, TVectorD &D, TMatrixD &Prt, TGraphAsymmErrors *box)
   TVectorD trialT(Nt);
   TVectorD trialR(D.GetNrows());
 
-  TTree *ptree = new TTree("tflat", 
+  TTree *ptree = new TTree("tflat",
                            "posterior probability from uniform sampling");
   for (int t=0; t<Nt; t++)
   {
@@ -138,18 +133,17 @@ SampleUniform(int nSamples, TVectorD &D, TMatrixD &Prt, TGraphAsymmErrors *box)
 }
 
 TTree *
-SampleMH(int nSamples, int nBurnIn, TVectorD &D, TMatrixD &Prt,
-         TGraphAsymmErrors *box, double alpha, TVectorD &Tmc)
+SampleMH(int nSamples, int nBurnIn, TMatrixD &Prt, TGraphAsymmErrors *box, 
+         LogLikeFn &llfunc, LogPrior &priorfunc)
 {
   // D = measured data, Prt = migration matrix, box = Nt-dim sampling volume.
   // Alpha and Tmc are used to construct the prior for regularization.
   int Nt = box->GetN();
-  float Tpoint[Nt], logL;
-  TRandom3 ran3;
-  TVectorD trialR(D.GetNrows());
-  TVectorD trialT(Nt);
-  TVectorD propT(Nt);
   double p0, p1;      // Current and proposed probabilities
+  float Tpoint[Nt], logL;
+  TVectorD trialT = MatrixUtils::Graph2Vec(box);
+  TVectorD trialR = Prt*trialT;
+  TVectorD propT(Nt);
 
   TTree *ptree = new TTree("tmcmc", "Metropolis-Hastings Markov chain");
   for (int t=0; t<Nt; t++)
@@ -158,87 +152,24 @@ SampleMH(int nSamples, int nBurnIn, TVectorD &D, TMatrixD &Prt,
   }
   ptree->Branch("logL", &logL, "logL/F");
 
-  // Set the first point as \tilde{T}
-  for (int t=0; t<Nt; t++)
-    trialT(t) = box->GetY()[t];
-
-  trialR = Prt*trialT;
-  p0 = LogPoissonLikelihood(D,trialR);
-
-  // Add regularization
-  if (alpha > 0)
-  {
-    for (int t=0; t<Nt; t++)
-    {
-      double arg = alpha*(trialT(t) - Tmc(t))/Tmc(t);
-      p0 -= 0.5*arg*arg;
-    }
-  }
+  // Note that llfunc < 0, priorfunc > 0.
+  p0 = llfunc(trialR) - priorfunc(trialT);
 
   std::cout << Form("Sampling L(D|T)*pi(T) using MCMC...")
             << std::endl;
 
   for (int i=0; i < nSamples + nBurnIn; i++)
   {
-
     PrintPercentDone(i, nSamples + nBurnIn, 1);
 
-    // Get a proposal point from a small box centered at the current
-    // point.
-    for (int t=0; t<Nt; t++)
-    {
-      double min = box->GetY()[t] - box->GetEYlow()[t];
-      double max = box->GetY()[t] + box->GetEYhigh()[t];
-      double dT = 0.01*(max - min);
-
-      // Make sure the proposal cell stays within the box, and that it
-      // always has the same volume dT^Nt.
-      double lo = TMath::Max(min, trialT(t) - dT/2);
-      double hi = TMath::Min(max, trialT(t) + dT/2);
-      if (lo == min)
-        hi = lo + dT;
-      if (hi == max)
-        lo = hi - dT;
-
-      propT(t) = ran3.Uniform(lo, hi);
-
-      if (false)    // Try Gaussian proposal instead
-      {
-        bool confirmedInside = false;
-        while (!confirmedInside)
-        {
-          propT(t) = ran3.Gaus(trialT(t), dT/2);
-          if ((propT(t) >= min) && (propT(t) <= max))
-            confirmedInside = true;
-        }
-      }
-
-      if (propT(t) < min)
-        Warning("","T(%d) = %f < %f", t, propT(t), min);
-      if (propT(t) > max)
-        Warning("","T(%d) = %f > %f", t, propT(t), max);
-    }
-
+    // Get a new proposal point (propT). Update trialR.
+    AssignProposal(box, trialT, propT);
     trialR = Prt*propT;
-    p1 = LogPoissonLikelihood(D,trialR);
 
-    // Add regularization
-    if (alpha > 0)
-    {
-      for (int t=0; t<Nt; t++)
-      {
-        double arg = alpha*(propT(t) - Tmc(t))/Tmc(t);
-        p1 -= 0.5*arg*arg;
-      }
-    }
+    // Note that llfunc < 0, priorfunc > 0.
+    p1 = llfunc(trialR) - priorfunc(propT);
 
-    bool accept = false;
-    if (p1 >= p0)
-      accept = true;
-    else if (TMath::Log(ran3.Uniform()) < p1-p0)
-      accept = true;
-
-    if (accept)
+    if (AcceptProposal(p0, p1))
     {
       logL = p0 = p1;
       trialT = propT;
@@ -250,94 +181,58 @@ SampleMH(int nSamples, int nBurnIn, TVectorD &D, TMatrixD &Prt,
         ptree->Fill();
     }
   }
-  Printf("Filled tree with %lld entries.",ptree->GetEntries());
+
+  Printf("Filled tree with %lld entries.", ptree->GetEntries());
 
   return ptree;
 }
 
-double
-PoissonLikelihood(TVectorD &b, TVectorD &R)
-{
 
-  double ans = 1;
-  for (int r=0; r<b.GetNrows(); ++r)
+void
+AssignProposal(const TGraphAsymmErrors *box, const TVectorD &currentvec,
+               TVectorD &newvec)
+{
+  static TRandom3 ran3;
+
+  // Get a proposal point from a small box centered at the current
+  // point. Assign the point to newvec.
+  for (int t=0; t<box->GetN(); t++)
   {
-    ans *= TMath::Poisson(b(r), R(r));
+    double min = box->GetY()[t] - box->GetEYlow()[t];
+    double max = box->GetY()[t] + box->GetEYhigh()[t];
+    double dT = 0.01*(max - min);
+
+    // Make sure the proposal cell stays within the box, and that it
+    // always has the same volume dT^Nt.
+    double lo = TMath::Max(min, currentvec(t) - dT/2);
+    double hi = TMath::Min(max, currentvec(t) + dT/2);
+    if (lo == min)
+      hi = lo + dT;
+    if (hi == max)
+      lo = hi - dT;
+
+    newvec(t) = ran3.Uniform(lo, hi);
+
+    if (newvec(t) < min)
+      Warning("","T(%d) = %f < %f", t, newvec(t), min);
+    if (newvec(t) > max)
+      Warning("","T(%d) = %f > %f", t, newvec(t), max);
   }
-  return ans;
+  return;
 }
 
-double
-LogPoissonLikelihood(TVectorD &b, TVectorD &R)
+bool
+AcceptProposal(double p0, double p1)
 {
-  double ans = 0;
-  for (int r=0; r<b.GetNrows(); ++r)
-  {
-    ans +=LogPoisson(b(r), R(r));
-  }
-  return ans;
-}
+  static TRandom3 ran3;
 
-double
-LogPoisson(double x, double mu)
-{
-  if (mu >= 1000)
-    return LogGaussian(x,mu,TMath::Sqrt(mu),true);
+  if (p1 >= p0)
+    return true;
 
-  if (x < 0)
-    return 0;
+  if (TMath::Log(ran3.Uniform()) < p1-p0)
+    return true;
 
-  if (x==0.)
-    return -mu;
-
-  return x*TMath::Log(mu) - mu - LogFactorial(int(x));
-}
-
-double
-LogGaussian(double x, double mu, double sigma, bool norm)
-{
-  if (sigma <= 0)
-    return 0;
-
-  double ans = -0.5*(x-mu)*(x-mu)/sigma/sigma;
-
-  if (norm)
-    ans -= TMath::Log(TMath::Sqrt(TMath::TwoPi())*sigma);
-
-  return ans;
-}
-
-double
-LogFactorial(int n)
-{
-  // Cache ln(n!) values for speed.
-  static int nStoredLnFac = 10000;
-  static double *lnFactorial = 0;
-
-  if (!lnFactorial)
-  {
-    // Compute & store ln n! on initial function call
-    if (0)
-    {
-      std::cout <<
-                Form("Pre-caching ln(n!) values up to n = %d...", nStoredLnFac)
-                << std::flush;
-    }
-
-    lnFactorial = new double[nStoredLnFac+1];
-
-    // Set ln(0) = 0 for stability
-    lnFactorial[0] = 0.;
-    for (int i=1; i<=nStoredLnFac; i++)
-      lnFactorial[i] = TMath::LnGamma(i+1);
-
-    //    Printf("Done.");
-  }
-
-  if (n<=nStoredLnFac)
-    return lnFactorial[n];
-
-  return  TMath::LnGamma(n+1);
+  return false;
 }
 
 BayesianCredibilityInterval
@@ -422,7 +317,6 @@ GetBCI(TH1 *hp, double probFrac)
       bci.u1 = cdf.GetX()[i] - 0.99*cdf.GetEX()[i];
       bci.u2 = cdf.GetX()[i2] + 0.99*cdf.GetEX()[i2];
     }
-
   }
 
   bci.u    = (bci.u1+bci.u2)/2;
