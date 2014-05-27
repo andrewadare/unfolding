a36eaa57bddc6ebda6d3f59d4fcb657a8f770088
Andrew Adare <andrewadare@gmail.com>
2014-05-27 14:50:45 -0600

    Massive refactoring to MatrixUtils and TestProblems

--- UnfoldingUtils.h
+++ UnfoldingUtils.h
@@ -15,49 +15,49 @@ class TF1;
 #include "TMatrixD.h"
 #include "TVectorD.h"
 
-struct QRDecompResult
-{
-  TMatrixD Q;
-  TMatrixD R;
-};
+// struct QRDecompResult
+// {
+//   TMatrixD Q;
+//   TMatrixD R;
+// };
 
-struct CSDecompResult
-{
-  TMatrixD C;
-  TMatrixD S;
-  TMatrixD U;
-  TMatrixD V;
-  TMatrixD Z;
-  TVectorD alpha;        // Diag(C). Size l (CSD) = n (GSVD).
-  TVectorD beta;         // Diag(S). Size l (CSD) = n (GSVD).
-};
+// struct CSDecompResult
+// {
+//   TMatrixD C;
+//   TMatrixD S;
+//   TMatrixD U;
+//   TMatrixD V;
+//   TMatrixD Z;
+//   TVectorD alpha;        // Diag(C). Size l (CSD) = n (GSVD).
+//   TVectorD beta;         // Diag(S). Size l (CSD) = n (GSVD).
+// };
 
-struct GSVDecompResult   // Output from GSVD().
-{
-  // Result of quotient aka generalized SVD of A,B.
-  // A = U*C*XT, B = V*S*XT.
-  TMatrixD C;
-  TMatrixD S;
-  TMatrixD U;
-  TMatrixD V;
-  TMatrixD XT;
+// struct GSVDecompResult   // Output from GSVD().
+// {
+//   // Result of quotient aka generalized SVD of A,B.
+//   // A = U*C*XT, B = V*S*XT.
+//   TMatrixD C;
+//   TMatrixD S;
+//   TMatrixD U;
+//   TMatrixD V;
+//   TMatrixD XT;
 
-  TVectorD alpha;        // Diag(C). Size n. Nonincreasing.
-  TVectorD beta;         // Diag(S). Size n. Nondecreasing.
-  TVectorD gamma;        // alpha/beta. Nonincreasing.
-};
+//   TVectorD alpha;        // Diag(C). Size n. Nonincreasing.
+//   TVectorD beta;         // Diag(S). Size n. Nondecreasing.
+//   TVectorD gamma;        // alpha/beta. Nonincreasing.
+// };
 
-struct TestProblem       // Output from test problem generator.
-{
-  TMatrixD R;            // Fine-binned response matrix.
-  TH2D* Response;        // Response matrix.
-  TH1D* xTruth;          // Discrete version of true PDF.
-  TH1D* xTruthEst;       // Estimator for xTruth.
-  TH1D* xIni;            // Model truth.
-  TH1D* bIdeal;          // Observed b, no noise.
-  TH1D* bNoisy;          // Observed b, perturbed by noise.
-  TH1D* eff;             // Efficiency: meas/true vs. true x
-};
+// struct TestProblem       // Output from test problem generator.
+// {
+//   TMatrixD R;            // Fine-binned response matrix.
+//   TH2D* Response;        // Response matrix.
+//   TH1D* xTruth;          // Discrete version of true PDF.
+//   TH1D* xTruthEst;       // Estimator for xTruth.
+//   TH1D* xIni;            // Model truth.
+//   TH1D* bIdeal;          // Observed b, no noise.
+//   TH1D* bNoisy;          // Observed b, perturbed by noise.
+//   TH1D* eff;             // Efficiency: meas/true vs. true x
+// };
 
 struct SVDResult         // Output from SVDAnalysis().
 {
@@ -112,6 +112,7 @@ class GSVDResult        // Output from GSVDAnalysis().
   TH1D* UTbAbs;          // Vector of |U'_i*b| values
   TH1D* coeffAbs;        // Vector of |U'_i*b|/alpha_i values
   TH1D* regcAbs;         // Regularized (filtered) coeffs.
+  // ClassDef(GSVDResult, 1);
 };
 
 struct UnfoldingResult
@@ -164,6 +165,7 @@ struct UnfoldingResult
   double lambdaRho;
   int kRho;
   TH1D* hRho; // Best unfolding result (according to GCV)
+  // ClassDef(UnfoldingResult, 1);
 };
 
 class UnfoldingUtils 
@@ -173,73 +175,74 @@ class UnfoldingUtils
   // Class administration
   UnfoldingUtils();
   UnfoldingUtils(TH2D* hA, 
-		 TH1D* hMeas, 
-		 TH2D* hMeasCov = 0, 
-		 TH1D* hXini    = 0, 
-		 TH1D* hXtrue   = 0, 
-		 TH1D* hEff     = 0);
+     TH1D* hMeas, 
+     TH2D* hMeasCov = 0, 
+     TH1D* hXini    = 0, 
+     TH1D* hXtrue   = 0, 
+     TH1D* hEff     = 0);
   virtual ~UnfoldingUtils() {}
   bool BinningOk();
   
   // Conversion methods
-  TVectorD Hist2Vec(const TH1* h, TString opt=""); // use "unc" to get error
-  TMatrixD Hist2Matrix(const TH2* h);
-  TVectorD Graph2Vec(const TGraph* g);
-  TH1D* Vec2Hist(const TVectorD& v, Double_t x1, Double_t x2, 
-		 TString name, TString title="");  
-  TH2D* Matrix2Hist(TMatrixD& A, TString hName,
-		    double x1, double x2, double y1, double y2);
-  TH2D* Matrix2Hist(TMatrixD& A, TString hName, 
-		    double xbins[], double ybins[]);
-  TH2D* Matrix2Hist(TMatrixD& A, TMatrixD& errA, TString hName, 
-		    double xbins[], double ybins[]);
+  // TVectorD Hist2Vec(const TH1* h, TString opt=""); // use "unc" to get error
+  // TMatrixD Hist2Matrix(const TH2* h);
+  // TVectorD Graph2Vec(const TGraph* g);
+  // TH1D* Vec2Hist(const TVectorD& v, Double_t x1, Double_t x2, 
+		//  TString name, TString title="");  
+  // TH2D* Matrix2Hist(TMatrixD& A, TString hName,
+		//     double x1, double x2, double y1, double y2);
+  // TH2D* Matrix2Hist(TMatrixD& A, TString hName, 
+		//     double xbins[], double ybins[]);
+  // TH2D* Matrix2Hist(TMatrixD& A, TMatrixD& errA, TString hName, 
+		//     double xbins[], double ybins[]);
   TH1D* XHist(TVectorD& x, TString base, int k, double xMin, 
 	      double xMax, double normto, TString opt="");
-  TH2D* BandedDiagonalMatrix(TH1* hDpt, 
-			     const int nMeas, 
-			     const int nTrue, 
-			     double xm1=0, 
-			     double xm2=0, 
-			     double xt1=0, 
-			     double xt2=0);
+  // TH2D* BandedDiagonalMatrix(TH1* hDpt, 
+		// 	     const int nMeas, 
+		// 	     const int nTrue, 
+		// 	     double xm1=0, 
+		// 	     double xm2=0, 
+		// 	     double xt1=0, 
+		// 	     double xt2=0);
 
-  // Matrix decompositions
-  QRDecompResult QRDecomp(TMatrixD& A);
-  QRDecompResult QLDecomp(TMatrixD& A);
-  CSDecompResult CSDecomp(TMatrixD& Q1, TMatrixD& Q2);
-  CSDecompResult CSDecompQ1Taller(TMatrixD& Q1, TMatrixD& Q2);
-  GSVDecompResult GSVD(TMatrixD& A, TMatrixD& B);
+  // // Matrix decompositions
+  // QRDecompResult QRDecomp(TMatrixD& A);
+  // QRDecompResult QLDecomp(TMatrixD& A);
+  // CSDecompResult CSDecomp(TMatrixD& Q1, TMatrixD& Q2);
+  // CSDecompResult CSDecompQ1Taller(TMatrixD& Q1, TMatrixD& Q2);
+  // GSVDecompResult GSVD(TMatrixD& A, TMatrixD& B);
 
-  // Utility methods
-  void ReverseColumns(TMatrixD& A); // Reverse all columns in A
-  void ReverseColumns(TMatrixD& A, int col1, int col2); // col1, col2 included
-  void ReverseRows(TMatrixD& A);
-  void ReverseVector(TVectorD& v);
-  void SwapColumns(TMatrixD &A, int col1, int col2);
-  void SwapElements(TVectorD& v, int j1, int j2);
-  void NormalizeXSum(TH2* hA, TH1* hN=0); // Modifies hA in-place
-  void NormalizeRows(TMatrixD& A, TVectorD& normto); // Modifies A in-place
-  TH2* TH2Product(TH2* hA, TH2* hB, TString name);
-  TH2D* TH2Sub(TH2* h, int bx1, int bx2, int by1, int by2, TString name);
+  // // Utility methods
+  // void ReverseColumns(TMatrixD& A); // Reverse all columns in A
+  // void ReverseColumns(TMatrixD& A, int col1, int col2); // col1, col2 included
+  // void ReverseRows(TMatrixD& A);
+  // void ReverseVector(TVectorD& v);
+  // void SwapColumns(TMatrixD &A, int col1, int col2);
+  // void SwapElements(TVectorD& v, int j1, int j2);
+  // void NormalizeXSum(TH2* hA, TH1* hN=0); // Modifies hA in-place
+  // void NormalizeRows(TMatrixD& A, TVectorD& normto); // Modifies A in-place
+  // TH2* TH2Product(TH2* hA, TH2* hB, TString name);
+  // TH2D* TH2Sub(TH2* h, int bx1, int bx2, int by1, int by2, TString name);
 
   TGraph* LogCurvature(TGraph* g, const TVectorD& tVec, int& kMax); // log curvature of parametric curve g(t). kMax is point index of greatest curvature.
+
   TMatrixD RegularizedInverseResponse(GSVDResult* gsvd, double lambda); // A^#
-  TMatrixD MoorePenroseInverse(TMatrixD& A, double tol = 1e-15); // Uses SVD
-  TMatrixD Null(TMatrixD& A); // Columns form a basis for the null space of A
-  int Rank(TMatrixD& A); // Uses SVD
-  TMatrixD Toeplitz(int m1, int n1, double col[], double row[]); // Pass in first col & row
+  // TMatrixD MoorePenroseInverse(TMatrixD& A, double tol = 1e-15); // Uses SVD
+  // TMatrixD Null(TMatrixD& A); // Columns form a basis for the null space of A
+  // int Rank(TMatrixD& A); // Uses SVD
+  // TMatrixD Toeplitz(int m1, int n1, double col[], double row[]); // Pass in first col & row
   TMatrixD LMatrix(const int n, const int kind, double eps = 1.e-5); // Matrix to define smoothing seminorm
-  TMatrixD DerivativeMatrix(int n, int d);
-  TVectorD Ones(int n); // n-vector of 1's
-  TVectorD ElemMult(const TVectorD& x, const TVectorD& y); // Element-wise vector multiplication
-  TVectorD ElemDiv (const TVectorD& x, const TVectorD& y, double div0val = 0.);  // Element-wise vector division
-  TMatrixD MultRowsByVector(const TMatrixD& M, const TVectorD& v);
-  TMatrixD DivRowsByVector(const TMatrixD& M, const TVectorD& v, bool makeZeroIfNaN=true); // Divide M rows by v elementwise: R(i,j) = M(i,j) / v(j).
-  TMatrixD DivColsByVector(const TMatrixD& M, const TVectorD& v, bool makeZeroIfNaN=true); // Divide M columns by v elementwise: R(i,j) = M(i,j) / v(i).
-  TMatrixD OuterProduct(TVectorD a, TVectorD b); // a*b'
-  void LTSolve(TVectorD& result, const TMatrixD& L, const TVectorD& y);
-  void LSolve(TVectorD& result, const TMatrixD& L, const TVectorD& y, 
-	      const TMatrixD& W, const TMatrixD& T);
+  // TMatrixD DerivativeMatrix(int n, int d);
+  // TVectorD Ones(int n); // n-vector of 1's
+  // TVectorD ElemMult(const TVectorD& x, const TVectorD& y); // Element-wise vector multiplication
+  // TVectorD ElemDiv (const TVectorD& x, const TVectorD& y, double div0val = 0.);  // Element-wise vector division
+  // TMatrixD MultRowsByVector(const TMatrixD& M, const TVectorD& v);
+  // TMatrixD DivRowsByVector(const TMatrixD& M, const TVectorD& v, bool makeZeroIfNaN=true); // Divide M rows by v elementwise: R(i,j) = M(i,j) / v(j).
+  // TMatrixD DivColsByVector(const TMatrixD& M, const TVectorD& v, bool makeZeroIfNaN=true); // Divide M columns by v elementwise: R(i,j) = M(i,j) / v(i).
+  // TMatrixD OuterProduct(TVectorD a, TVectorD b); // a*b'
+  // void LTSolve(TVectorD& result, const TMatrixD& L, const TVectorD& y);
+  // void LSolve(TVectorD& result, const TMatrixD& L, const TVectorD& y, 
+	 //      const TMatrixD& W, const TMatrixD& T);
   void SetTH1Props(TH1* h,
 		   Int_t linecolor,
 		   Int_t fillcolor,
@@ -253,28 +256,28 @@ class UnfoldingUtils
   // C. B. Shaw, Jr., "Improvements of the resolution of an instrument
   // by numerical solution of an integral equation", J. Math. Anal. Appl. 37
   // (1972), 83–112.
-  TestProblem ShawSystem(const int n, double noise=0.);
-  void ShawSystem(const int n, TMatrixD& A, TVectorD& x, TVectorD& b,
-		  double noise=0);
+  // TestProblem ShawSystem(const int n, double noise=0.);
+  // void ShawSystem(const int n, TMatrixD& A, TVectorD& x, TVectorD& b,
+		//   double noise=0);
 
-  TestProblem MonteCarloConvolution(const int m, 
-				    const int n,
-				    const double xm1,
-				    const double xm2,
-				    const double xt1,
-				    const double xt2,
-				    TF1* truthFn, 
-				    TF1* kernelFn, 
-				    const int nEvents);
+  // TestProblem MonteCarloConvolution(const int m, 
+		// 		    const int n,
+		// 		    const double xm1,
+		// 		    const double xm2,
+		// 		    const double xt1,
+		// 		    const double xt2,
+		// 		    TF1* truthFn, 
+		// 		    TF1* kernelFn, 
+		// 		    const int nEvents);
   
-  TestProblem AtlasDiJetMass(const int Nt, 
-			     const int Nr,
-			     double tbins[],
-			     double rbins[],
-			     const double apar = 0.5,
-			     const double bpar = 0.1,
-			     const int nEvents = int(1e7),
-			     const double evtWeight = 1e-3);
+  // TestProblem AtlasDiJetMass(const int Nt, 
+		// 	     const int Nr,
+		// 	     double tbins[],
+		// 	     double rbins[],
+		// 	     const double apar = 0.5,
+		// 	     const double bpar = 0.1,
+		// 	     const int nEvents = int(1e7),
+		// 	     const double evtWeight = 1e-3);
 
   // Unfolding methods:
   // Preconditioned Conjugate Gradients for Least Squares
@@ -424,6 +427,6 @@ class UnfoldingUtils
   TVectorD fVecXini;    // Model truth distribution
   TVectorD fSmoothingWeight;
 
-  ClassDef(UnfoldingUtils, 1);
+  // ClassDef(UnfoldingUtils, 1);
 };
 #endif
